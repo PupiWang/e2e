407f608de35f3eef41ee33d120c5295f84d1afe5
Pupi <dreamjl@live.cn>
2015-09-21 21:30:42 +0800

    add crypt util.

---
+++ test/e2e/utils/encrypt_decrypt.js
@@ -0,0 +1,18 @@
+var crypto = require('crypto');
+
+module.exports = {
+    encrypt: function (text) {
+        var cipher = crypto.createCipher('aes-256-cbc', 'd6F3Efeq');
+        var crypted = cipher.update(text, 'utf8', 'hex');
+        crypted += cipher.final('hex');
+        return crypted;
+    },
+    decrypt: function (text) {
+        var decipher = crypto.createDecipher('aes-256-cbc', 'd6F3Efeq');
+        var dec = decipher.update(text, 'hex', 'utf8');
+        dec += decipher.final('utf8');
+        return dec;
+    }
+};
+
+console.log(module.exports.encrypt('123456'));
